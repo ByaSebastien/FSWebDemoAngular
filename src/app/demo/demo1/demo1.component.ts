41bf7a14b05b0d9bde2322ed4d7b891e2249a4b3
Sebastien <byasebastien@hotmail.com>
2023-08-30 12:13:27 +0200

    first commit

--- src/app/demo/demo1/demo1.component.ts
+++ src/app/demo/demo1/demo1.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo1.component.scss']
 })
 export class Demo1Component {
+  // Property binding
+  prenom: string = "Seb"
+  prenom2: string = ""
 
+  //Event binding
+  jour: string = ""
+
+  //Attribute binding
+  id: string = "ma-div"
+  isActive: boolean = true
+
+  //Méthodes
+  changerJour(j: string): void {
+    this.jour = j;
+  }
+
+  toggleActive(): void {
+    this.isActive = !this.isActive;
+  }
 }
