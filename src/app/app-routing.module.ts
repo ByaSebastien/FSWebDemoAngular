4c856deffa7086cc23ec31c2c6d9a89b02bca3b3
Sebastien <byasebastien@hotmail.com>
2023-08-30 17:04:55 +0200

    chronometre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { Demo1Component } from './demo/demo1/demo1.component';
 import { Exercice1Component } from './exercices/exercice1/exercice1.component';
 import { Demo2Component } from './demo/demo2/demo2.component';
+import { Exercice2Component } from './exercices/exercice2/exercice2.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'demo1', component: Demo1Component },
   { path: 'demo2', component: Demo2Component },
-  { path: 'exercice1', component: Exercice1Component }
+  { path: 'exercice1', component: Exercice1Component },
+  { path: 'exercice2', component: Exercice2Component }
 ];
 
 @NgModule({
--- src/app/demo/demo2/demo2.component.ts
+++ src/app/demo/demo2/demo2.component.ts
@@ -11,4 +11,27 @@ export class Demo2Component {
   myDate: Date = new Date();
   myTempC: number = 16;
   myTempF: number = 42;
+
+  timer: any = undefined;
+
+  start(): void {
+    this.timer = setInterval(
+      () => {
+        this.myNumber = this.myNumber + 1;
+      }, 1000
+    )
+  }
+
+  stop(): void {
+    clearInterval(this.timer);
+  }
+
+  reset(): void {
+    this.myNumber = 0;
+  }
+
+  methodTest(name: string, age: number): void {
+    console.log("Bonjour " + name + " tu as " + age + " an(s)");
+
+  }
 }
